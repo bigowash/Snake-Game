cd659c9a94243ef7973f73fc67cd3aea8470a9fd
bigowash <bui.theo@gmail.com>
2022-10-11 13:53:43 +0100

    feat : finished basic game

--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
-var s;
-var scl = 20;
+let s;
+const scl = 20;
+
+
+let food;
 
 function setup() {
     createCanvas(600, 600);
     s = new Snake();
+    frameRate(10);
+    pickLocation();
+}
+
+function pickLocation() {
+    const cols = floor(width / scl);
+    const rows = floor(height / scl);
+    food = createVector(ceil(random(cols)), ceil(random(rows)));
+    food.mult(scl);
+}
+
+function mousePressed() {
+    s.total++
 }
 
 function draw() {
     background(51);
+    s.death();
     s.update();
     s.show();
+
+    if (s.eat(food)) {
+        pickLocation();
+        // s.total++
+    }
+
+    fill(255, 0, 100);
+    rect(food.x, food.y, scl, scl)
 }
 
 function keyPressed() {
--- snake.js
+++ snake.js
@@ -3,19 +3,58 @@ function Snake() {
     this.y = 0
     this.xspeed = 1;
     this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
 
     this.dir = function (x, y) {
         this.xspeed = x;
         this.yspeed = y;
     }
 
+    this.eat = function (pos) {
+        const d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
+            this.total++;
+            return true;
+        }
+        return false
+    }
+
+    this.death = function () {
+
+        for (const pos of this.tail) {
+            const d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                console.log("starting over again");
+                this.total = 0;
+                this.tail = [];
+            }
+        }
+    }
+
     this.update = function () {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
+
+        if (this.total === this.tail.length) {
+            for (let i = 0; i < this.tail.length - 1; i++) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
+        this.tail[this.total - 1] = createVector(this.x, this.y);
+
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl)
+        this.y = constrain(this.y, 0, height - scl)
     }
 
     this.show = function () {
         fill(255);
-        rect(this.x, this.y, 10, 10)
+
+        for (let i = 0; i < this.total; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl)
+        }
+
+        rect(this.x, this.y, scl, scl)
     }
 }
